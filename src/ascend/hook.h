080894f77f07cd322237fcb7c1bd3f1bdf8817f0
weli-l <1289113577@qq.com>
2025-04-16 10:30:25 +0800

    add rt
    Signed-off-by: weli-l <1289113577@qq.com>

--- src/ascend/hook.h
+++ src/ascend/hook.h
@@ -11,16 +11,16 @@
 extern "C" {
 #endif
 
-typedef aclError (*aclLaunchKernelFn)(aclrtFuncHandle, uint32_t, const void*, size_t, aclrtStream);
+typedef aclError (*aclrtLaunchKernelFn)(aclrtFuncHandle, uint32_t, const void*, size_t, aclrtStream);
 
-extern aclLaunchKernelFn orig_aclrtLaunchKernel;
+extern aclrtLaunchKernelFn orig_aclrtLaunchKernel;
 
 EXPOSE_API aclError aclrtLaunchKernel(aclrtFuncHandle funcHandle, 
                                     uint32_t blockDim,
                                     const void* argsData,
                                     size_t argsSize,
                                     aclrtStream stream);
-aclLaunchKernelFn orig_aclrtLaunchKernel = nullptr;
+aclrtLaunchKernelFn orig_aclrtLaunchKernel = nullptr;
 
 
 #ifdef __cplusplus
