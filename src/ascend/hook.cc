7bb8aa4bee6e110ddecf75e7653a2a88a317d0b7
weli-l <1289113577@qq.com>
2025-04-17 10:10:41 +0800

    add aclrtMapMem for test
    Signed-off-by: weli-l <1289113577@qq.com>

--- src/ascend/hook.cc
+++ src/ascend/hook.cc
@@ -6,6 +6,24 @@ extern "C" {
 #endif
 
 EXPOSE_API 
+aclError aclrtMapMem(void* virPtr, size_t size, size_t offset, 
+                    aclrtDrvMemHandle handle, uint64_t flags) {
+    try {
+        std::cout << "[SYSTRACE] Entering aclrtMapMem" << std::endl;
+        SETUP_DLSYM(aclrtMapMem);
+        if (!::systrace::util::config::GlobalConfig::enable)
+            return orig_aclrtMapMem(virPtr, size, offset, handle, flags);
+        ::systrace::SysTrace::getInstance();   
+        std::cout << "[SYSTRACE] SysTrace instance created" << std::endl;
+        } catch (...) {
+            std::cout << "[SYSTRACE] Exception in aclrtMapMem" << std::endl;
+            return orig_aclrtMapMem(virPtr, size, offset, handle, flags);
+        }
+    std::cout << "[SYSTRACE] Calling original aclrtMapMem" << std::endl;
+    return orig_aclrtMapMem(virPtr, size, offset, handle, flags);
+}
+
+EXPOSE_API 
 aclError aclrtLaunchKernel(aclrtFuncHandle funcHandle, 
                          uint32_t blockDim,
                          const void* argsData,
--- src/ascend/hook.h
+++ src/ascend/hook.h
@@ -11,17 +11,21 @@
 extern "C" {
 #endif
 
+typedef aclError (*aclrtMapMemFn)(void*, size_t, size_t, aclrtDrvMemHandle, uint64_t);
 typedef aclError (*aclrtLaunchKernelFn)(aclrtFuncHandle, uint32_t, const void*, size_t, aclrtStream);
 
+extern aclrtMapMemFn orig_aclrtMapMem;
 extern aclrtLaunchKernelFn orig_aclrtLaunchKernel;
 
+EXPOSE_API aclError aclrtMapMem(void* virPtr, size_t size, size_t offset, 
+                              aclrtDrvMemHandle handle, uint64_t flags);
 EXPOSE_API aclError aclrtLaunchKernel(aclrtFuncHandle funcHandle, 
                                     uint32_t blockDim,
                                     const void* argsData,
                                     size_t argsSize,
                                     aclrtStream stream);
+aclrtMapMemFn orig_aclrtMapMem = nullptr;
 aclrtLaunchKernelFn orig_aclrtLaunchKernel = nullptr;
-
 
 #ifdef __cplusplus
 }
