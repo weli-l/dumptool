cf7b4e2cfbbbd28a2bce33254ff564c9665457d6
weli-l <1289113577@qq.com>
2025-04-17 19:40:51 +0800

    add log
    Signed-off-by: weli-l <1289113577@qq.com>

--- include/common/util.cc
+++ include/common/util.cc
@@ -202,92 +202,92 @@ void setUpConfig() {
 }
 
 void setUpGlobalConfig() {
-  LOG(INFO) << "Entering setUpGlobalConfig()";
+  LOG(INFO) << "Entering setUpGlobalConfig()" << std::endl;
 
   try {
-    LOG(INFO) << "Getting RANK environment variable";
+    LOG(INFO) << "Getting RANK environment variable" << std::endl;
     GlobalConfig::rank = EnvVarRegistry::GetEnvVar<int>("RANK");
-    LOG(INFO) << "RANK = " << GlobalConfig::rank;
+    LOG(INFO) << "RANK = " << GlobalConfig::rank << std::endl;
 
-    LOG(INFO) << "Getting ENV_ARGO_WORKFLOW_NAME environment variable";
+    LOG(INFO) << "Getting ENV_ARGO_WORKFLOW_NAME environment variable" << std::endl;
     GlobalConfig::job_name = EnvVarRegistry::GetEnvVar<std::string>("ENV_ARGO_WORKFLOW_NAME");
-    LOG(INFO) << "ENV_ARGO_WORKFLOW_NAME = " << GlobalConfig::job_name;
+    LOG(INFO) << "ENV_ARGO_WORKFLOW_NAME = " << GlobalConfig::job_name << std::endl;
 
-    LOG(INFO) << "Getting LOCAL_RANK environment variable";
+    LOG(INFO) << "Getting LOCAL_RANK environment variable" << std::endl;
     GlobalConfig::local_rank = EnvVarRegistry::GetEnvVar<int>("LOCAL_RANK");
-    LOG(INFO) << "LOCAL_RANK = " << GlobalConfig::local_rank;
+    LOG(INFO) << "LOCAL_RANK = " << GlobalConfig::local_rank << std::endl;
 
-    LOG(INFO) << "Getting LOCAL_WORLD_SIZE environment variable";
+    LOG(INFO) << "Getting LOCAL_WORLD_SIZE environment variable" << std::endl;
     GlobalConfig::local_world_size = EnvVarRegistry::GetEnvVar<int>("LOCAL_WORLD_SIZE");
-    LOG(INFO) << "LOCAL_WORLD_SIZE = " << GlobalConfig::local_world_size;
+    LOG(INFO) << "LOCAL_WORLD_SIZE = " << GlobalConfig::local_world_size << std::endl;
 
-    LOG(INFO) << "Getting WORLD_SIZE environment variable";
+    LOG(INFO) << "Getting WORLD_SIZE environment variable" << std::endl;
     GlobalConfig::world_size = EnvVarRegistry::GetEnvVar<int>("WORLD_SIZE");
-    LOG(INFO) << "WORLD_SIZE = " << GlobalConfig::world_size;
+    LOG(INFO) << "WORLD_SIZE = " << GlobalConfig::world_size << std::endl;
 
     GlobalConfig::rank_str = "[RANK " + std::to_string(GlobalConfig::rank) + "] ";
-    LOG(INFO) << "rank_str = " << GlobalConfig::rank_str;
+    LOG(INFO) << "rank_str = " << GlobalConfig::rank_str << std::endl;
 
-    LOG(INFO) << "Getting SYSTRACE_DEBUG_MODE environment variable";
+    LOG(INFO) << "Getting SYSTRACE_DEBUG_MODE environment variable" << std::endl;
     GlobalConfig::debug_mode = EnvVarRegistry::GetEnvVar<bool>("SYSTRACE_DEBUG_MODE");
-    LOG(INFO) << "SYSTRACE_DEBUG_MODE = " << GlobalConfig::debug_mode;
+    LOG(INFO) << "SYSTRACE_DEBUG_MODE = " << GlobalConfig::debug_mode << std::endl;
 
     std::string dev_path = "/dev/davinci";
-    LOG(INFO) << "Starting device search in " << dev_path << "0-15";
+    LOG(INFO) << "Starting device search in " << dev_path << "0-15" << std::endl;
 
     for (uint64_t device_index = 0; device_index < 16; device_index++) {
       std::filesystem::path dev(dev_path + std::to_string(device_index));
-      LOG(INFO) << "Checking device: " << dev;
+      LOG(INFO) << "Checking device: " << dev << std::endl;
       if (std::filesystem::exists(dev)) {
         GlobalConfig::all_devices.push_back(device_index);
-        LOG(INFO) << "Found device: " << dev;
+        LOG(INFO) << "Found device: " << dev << std::endl;
         if (GlobalConfig::local_rank == 0) {
-          LOG(INFO) << "[ENV] Found device " << dev;
+          LOG(INFO) << "[ENV] Found device " << dev << std::endl;
         }
       } else {
-        LOG(INFO) << "Device not found: " << dev;
+        LOG(INFO) << "Device not found: " << dev << std::endl;
       }
     }
     
-    LOG(INFO) << "Found " << GlobalConfig::all_devices.size() << " devices";
+    LOG(INFO) << "Found " << GlobalConfig::all_devices.size() << " devices" << std::endl;
     if (GlobalConfig::all_devices.empty()) {
       GlobalConfig::enable = false;
-      LOG(WARNING) << "[ENV] No devices found, disabling tracing";
+      LOG(WARNING) << "[ENV] No devices found, disabling tracing" << std::endl;
     } else {
-      LOG(INFO) << "Device list:";
+      LOG(INFO) << "Device list:" << std::endl;
       for (auto dev : GlobalConfig::all_devices) {
-        LOG(INFO) << "  " << dev_path << dev;
+        LOG(INFO) << "  " << dev_path << dev << std::endl;
       }
     }
     
     std::sort(GlobalConfig::all_devices.begin(), GlobalConfig::all_devices.end());
-    LOG(INFO) << "Sorted device list:";
+    LOG(INFO) << "Sorted device list:" << std::endl;
     for (auto dev : GlobalConfig::all_devices) {
-      LOG(INFO) << "  " << dev_path << dev;
+      LOG(INFO) << "  " << dev_path << dev << std::endl;
     }
 
     LOG(INFO) << "Comparing local_world_size (" << GlobalConfig::local_world_size 
-              << ") with found devices (" << GlobalConfig::all_devices.size() << ")";
+              << ") with found devices (" << GlobalConfig::all_devices.size() << ")" << std::endl;
     if (GlobalConfig::local_world_size != GlobalConfig::all_devices.size()) {
       LOG(WARNING) << "[ENV] local world size(" << GlobalConfig::local_world_size
                 << ") is not equal to found devices("
-                << GlobalConfig::all_devices.size() << ") disabling hook";
+                << GlobalConfig::all_devices.size() << ") disabling hook" << std::endl;
       GlobalConfig::enable = false;
     }
 
-    LOG(INFO) << "Current enable flag: " << GlobalConfig::enable;
+    LOG(INFO) << "Current enable flag: " << GlobalConfig::enable << std::endl;
     if (!GlobalConfig::enable) {
-      LOG(INFO) << "[ENV] Not all devices are used, disable hook";
+      LOG(INFO) << "[ENV] Not all devices are used, disable hook" << std::endl;
     }
     if (GlobalConfig::debug_mode) {
       GlobalConfig::enable = true;
-      LOG(INFO) << "[ENV] Debug mode is on, ignore all checks";
+      LOG(INFO) << "[ENV] Debug mode is on, ignore all checks" << std::endl;
     }
     
-    LOG(INFO) << "Final enable flag: " << GlobalConfig::enable;
-    LOG(INFO) << "Exiting setUpGlobalConfig() successfully";
+    LOG(INFO) << "Final enable flag: " << GlobalConfig::enable << std::endl;
+    LOG(INFO) << "Exiting setUpGlobalConfig() successfully" << std::endl;
   } catch (const std::exception& e) {
-    LOG(ERROR) << "Failed to initialize global config: " << e.what();
+    LOG(ERROR) << "Failed to initialize global config: " << e.what() << std::endl;
     throw;
   }
 }
