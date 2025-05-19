68f483666b863eb0e3cfae0585580063dd10afb0
weli-l <1289113577@qq.com>
2025-05-19 17:02:03 +0800

    modify build.sh
    Signed-off-by: weli-l <1289113577@qq.com>

--- sysTrace/build.sh
+++ sysTrace/build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+
+sudo dnf remove -y libunwind libunwind-devel 2>/dev/null || true
 mkdir -p build
 
 cd protos
-
 protoc --c_out=. systrace.proto
-protoc --cpp_out=. systrace.proto 
+protoc --cpp_out=. systrace.proto
 protoc --python_out=. systrace.proto
+cd ..
 
-cd ../build
+mkdir -p libunwind_build
+cd libunwind_build
+git clone https://github.com/libunwind/libunwind.git
+cd libunwind
+autoreconf -i
+./configure --prefix=/usr/local
+make -j $(nproc) 
+sudo make install
+cp -r include /usr/local
+cd ../..
 
+cd build
 cmake ..
-make -j 50
+make -j $(nproc)
