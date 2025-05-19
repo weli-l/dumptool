e4370c3624d2b90da8372777bda5c34f0f533cc5
weli-l <1289113577@qq.com>
2025-05-19 20:32:40 +0800

    remove git clone libunwind in build.sh
    Signed-off-by: weli-l <1289113577@qq.com>

--- sysTrace/build.sh
+++ sysTrace/build.sh
@@ -8,18 +8,6 @@ protoc --c_out=. systrace.proto
 protoc --cpp_out=. systrace.proto
 protoc --python_out=. systrace.proto
 cd ..
-
-mkdir -p libunwind_build
-cd libunwind_build
-git clone https://github.com/libunwind/libunwind.git
-cd libunwind
-autoreconf -i
-./configure --prefix=/usr/local
-make -j $(nproc) 
-sudo make install
-cp -r include /usr/local
-cd ../..
-
 cd build
 cmake ..
 make -j $(nproc)
